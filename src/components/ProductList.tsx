aaf802ae9fc84614973c8f43d0a675e74ef30af3
Bhawani1200 <bhawanilimbu1200@gmail.com>
2024-12-03 21:15:49 +0545

    ..

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,128 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 flex-wrap justify-between">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="/dhaka9.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="/dhaka11.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium text-black">Product Name</span>
+          <span className="font-semibold text-black">$45</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-lama w-max py-2 px-4 text-xs text-black hover:bg-lama hover:text-white">
+          Add to cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="/dhaka12.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="/dhaka13.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium text-black">Product Name</span>
+          <span className="font-semibold text-black">$45</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-lama w-max py-2 px-4 text-xs text-black hover:bg-lama hover:text-white">
+          Add to cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="/dhaka1.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="/dhaka2.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium text-black">Product Name</span>
+          <span className="font-semibold text-black">$45</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-lama w-max py-2 px-4 text-xs text-black hover:bg-lama hover:text-white">
+          Add to cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="/dhaka7.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="/dhaka8.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium text-black">Product Name</span>
+          <span className="font-semibold text-black">$45</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-lama w-max py-2 px-4 text-xs text-black hover:bg-lama hover:text-white">
+          Add to cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,13 +1,14 @@
 "use client";
+import { transform } from "next/dist/build/swc/generated-native";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 const slides = [
   {
     id: 1,
     title: "Summer Sale Collection",
-    description: "sale! ,Upto 50% off!",
-    img: "/dhaka1.jpg",
+    description: "Sale! ,Upto 50% off!",
+    img: "/dhaka8.jpg",
     url: "/",
     bg: "bg-gradient-to-r form-yellow-50 to-pink-50",
   },
@@ -15,7 +16,7 @@ const slides = [
     id: 2,
     title: "Winter Sale Collection",
     description: "Sale! ,Upto 50% off!",
-    img: "/dhaka2.jpg",
+    img: "/dhaka5.jpg",
     url: "/",
     bg: "bg-gradient-to-r form-pink-50 to-blue-50",
   },
@@ -23,24 +24,43 @@ const slides = [
     id: 3,
     title: "Spring Sale Collection",
     description: "Sale! ,Upto 50% off!",
-    img: "/dhaka3.jpg",
+    img: "/dhaka6.jpg",
     url: "/",
     bg: "bg-gradient-to-r form-blue-50 to-yellow-50",
   },
 ];
 const Slider = () => {
   const [current, setCurrent] = useState(0);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+  //   }, 3000);
+  //   return () => clearInterval(interval);
+  // }, []);
   return (
     <div className="h-[calc(100vh-80px)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000">
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
+      >
         {slides.map((slide) => (
-          <div className="" key={slide.id}>
+          <div
+            className={`${slide.bg} h-full w-screen flex flex-col gap-16 xl:flex-row`}
+            key={slide.id}
+          >
             {/* TEXT CONTAINER */}
-            <div className="w-1/2">
-              <h2 className="text-black">{slide.description}</h2>
-              <h1 className="text-black">{slide.title}</h1>
+            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center ">
+              <h2 className="text-black text-xl lg:text-3xl 2xl:text-5xl">
+                {slide.description}
+              </h2>
+              <h1 className="text-black text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                {slide.title}
+              </h1>
               <Link href={slide.url}>
-                <button className="text-black">Shop Now</button>
+                <button className="text-white rounded-md bg-black py-3 px-4">
+                  Shop Now
+                </button>
               </Link>
             </div>
             {/* IMAGE CONTAINER */}
@@ -53,6 +73,21 @@ const Slider = () => {
                 className="object-cover"
               />
             </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+        {slides.map((slide, index) => (
+          <div
+            className={`w-2 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150 " : ""
+            }`}
+            key={slide.id}
+            onClick={() => setCurrent(index)}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
           </div>
         ))}
       </div>
