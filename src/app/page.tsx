d78669e6c3113c6d12be0ccd64744ad03f8c0e17
Bhawani1200 <bhawanilimbu1200@gmail.com>
2024-12-04 08:57:16 +0545

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import CategoryList from "@/components/CategoryList";
+import Footer from "@/components/Footer";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 
@@ -7,6 +9,16 @@ export default function Home() {
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 ">
         <h1 className="text-2xl">Featured Product</h1>
+        <ProductList />
+      </div>
+      <div className="mt-24">
+        <h1 className="text-2xl text-black px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+        <CategoryList />
+      </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 ">
+        <h1 className="text-2xl text-black">New Product</h1>
         <ProductList />
       </div>
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div>
-      footer
+  return;
+  <div className="">
+    {/* TOP */}
+    <div className="">
+      {/* LEFT */}
+      <div className="w-1/4 ">
+        <Link href="">
+          <Image src="/logo.png" alt="" height={200} width={200} />
+        </Link>
+        <p>Dharan,Sunsari Nepal</p>
+        <span className="text-semibold">chauiseydhaka12@gmail.com</span>
+        <span className="text-semibold">025-23456</span>
+        <div className="flex gap-6">
+        <Image src="/facebook.png" alt="" height={16} width={16} />
+        <Image src="/instagram.png" alt="" height={16} width={16} />
+        <Image src="/youtube.png" alt="" height={16} width={16} />
+        <Image src="/tiktok.png" alt="" height={16} width={16} />
+        </div>
+      </div>
+      {/* CENTER */}
+      <div className="w-1/2 "></div>
+      {/* RIGHT */}
+      <div className="w-1/4 "></div>
     </div>
-  )
-}
+    {/* BOTTOM */}
+    <div className=""></div>
+  </div>;
+};
 
-export default Footer
+export default Footer;
